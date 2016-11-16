e9c81438f2649284a7aa915aa1aa7bc86a12f573
Luke Taylor <luke.taylor@guardian.co.uk>
2016-11-16 10:41:47 +0000

    Clean standfirst.

--- restaurants/src/main/scala/com/gu/restaurant_review_parser/parsers/Parser.scala
+++ restaurants/src/main/scala/com/gu/restaurant_review_parser/parsers/Parser.scala
@@ -4,13 +4,14 @@ import java.time.OffsetDateTime
 
 import com.gu.contentapi.client.model.v1.CapiDateTime
 import com.gu.restaurant_review_parser._
+import org.jsoup.Jsoup
 
 object Parser {
 
   trait RestaurantReviewerBasedParser[T] {
 
     val reviewer: String
-    def guessReviewSnippet(standfirst: Option[Standfirst]): Option[ReviewSnippet] = standfirst.map(sf => ReviewSnippet(sf.value.stripSuffix("'").stripPrefix("'")))
+    def guessReviewSnippet(standfirst: Option[Standfirst]): Option[ReviewSnippet] = standfirst.map(sf => ReviewSnippet(Jsoup.parse(sf.value.stripSuffix("'").stripPrefix("'")).text))
     def creationDate(creationDate: Option[CapiDateTime]) = creationDate.map(time => OffsetDateTime.parse(time.iso8601))
     def publicationDate(webPublicationDate: Option[CapiDateTime]) = webPublicationDate.map(time => OffsetDateTime.parse(time.iso8601)).getOrElse(OffsetDateTime.now)
     def guessRestaurantWebAddress(articleBody: ArticleBody, restaurantName: RestaurantName): Option[WebAddress]
