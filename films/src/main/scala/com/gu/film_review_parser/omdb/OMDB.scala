f471b6b3597f4634d41dfd2399d4dc6b410807ed
tomrf1 <tom.forbes@theguardian.com>
2017-02-07 10:58:24 +0000

    Support circe 0.6.1

--- films/src/main/scala/com/gu/film_review_parser/omdb/OMDB.scala
+++ films/src/main/scala/com/gu/film_review_parser/omdb/OMDB.scala
@@ -6,6 +6,7 @@ import java.util.concurrent.TimeUnit
 import com.squareup.okhttp.{OkHttpClient, Request}
 import io.circe.parser._
 import io.circe.generic.auto._
+import cats.syntax.either._
 
 case class OMDBData(genre: List[String],
                     year: Short,
@@ -29,17 +30,17 @@ object OMDB {
     val request = new Request.Builder().url(buildUrl(title)).build
     val response = httpClient.newCall(request).execute
     if (response.isSuccessful) {
-      parse(response.body.string).flatMap(json => json.as[OMDBResponse]).fold(
-        { error =>
+      parse(response.body.string).toOption.map(json => json.as[OMDBResponse]).flatMap {
+        case Left(error) =>
           println(s"Error processing OMDB response json for title $title: $error")
           None
-        }, { responseData =>
+        case Right(responseData) =>
           val directors = List(responseData.Director)
           val actors = responseData.Actors.split(",").map(_.trim).toList
           val genres = responseData.Genre.split(",").map(_.trim).toList
           Some(OMDBData(genres, responseData.Year, responseData.imdbID, directors, actors))
-        }
-      )
+      }
+
     } else {
       println(s"Unable to get data from OMDB for title $title. Status code was: ${response.code}, body was: ${response.body.string}")
       None
